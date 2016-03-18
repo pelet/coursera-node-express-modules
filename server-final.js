99ff5bb0b9751481a1618cf7dfd41b26464dd0e4
pelet <tilli.tuomo@gmail.com>
2016-03-19 00:27:36 +0200

    dish and promotions modules added

---
+++ server-final.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var morgan = require('morgan');
+
+var dishRouter = require('./dishRouter.js');
+var promoRouter = require('./promotions.js');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function() {
+	console.log(`Server running at http://${hostname}:${port}`);
+});
